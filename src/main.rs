7509af2034f36ba89615fa4db88afa0bb6b3c844
Adrian <adrian.tubio@outlook.com>
2024-07-10 01:06:27 +0800

    Add support for fetching files

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::{
+    fs,
     io::{Read, Write},
     net::{TcpListener, TcpStream},
+    path::PathBuf,
     thread,
 };
 
@@ -10,7 +12,7 @@ fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
     for stream in listener.incoming() {
-        thread::spawn(move || {
+        thread::spawn(|| {
             let buff: &mut [u8] = &mut [0; 100];
             match stream {
                 Ok(mut _stream) => {
@@ -55,6 +57,7 @@ fn read_to_buffer(mut stream: &TcpStream, buf: &mut [u8]) {
         .collect::<Vec<String>>();
 
     let response = match &request_line[1] {
+        file_fetch if file_fetch.starts_with("/files/") => file_fetch_handler(file_fetch),
         user_agent if user_agent == "/user-agent" => user_agent_handler(&header),
         echo if echo.starts_with("/echo/") => {
             let data = echo.trim_start_matches("/echo/");
@@ -73,6 +76,43 @@ fn read_to_buffer(mut stream: &TcpStream, buf: &mut [u8]) {
     };
 
     let _ = stream.write_all(response.as_bytes());
+}
+
+fn file_fetch_handler(file_fetch: &str) -> String {
+    // let prefix = "/tmp/";
+    let prefix = std::env::args().nth(2).expect("test");
+    let data = file_fetch.trim_start_matches("/files/");
+    let file_path = PathBuf::from(format!("{}{}", prefix, data));
+    let canonical_path_result = fs::canonicalize(file_path);
+
+    let canonical_path = match canonical_path_result {
+        Err(_) => return not_found(),
+        Ok(path) => {
+            // if !path.starts_with(prefix) {
+            //     return not_found();
+            // }
+            path
+        }
+    };
+
+    let mut buf: Vec<u8> = Vec::new();
+    let file = fs::File::open(canonical_path);
+
+    let _ = match file {
+        Ok(mut f) => f.read_to_end(&mut buf),
+        _ => return not_found(),
+    };
+
+    let body = match std::str::from_utf8(&buf) {
+        Ok(b) => b,
+        _ => return not_found(),
+    };
+
+    format!(
+        "HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: {}\r\n\r\n{}",
+        buf.len(),
+        body
+    )
 }
 
 fn user_agent_handler(header: &str) -> String {
@@ -106,4 +146,8 @@ fn get_body<'a>(r: &'a str) -> (&str, &str) {
         Some((header, body)) => (header, body),
         None => ("", ""),
     }
+}
+
+fn not_found() -> String {
+    "HTTP/1.1 404 Not Found\r\n\r\n".into()
 }
